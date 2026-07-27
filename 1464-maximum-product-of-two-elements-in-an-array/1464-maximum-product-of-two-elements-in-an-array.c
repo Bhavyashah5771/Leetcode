c00b84adfa02fecadfa78bf2425178def382b019
Bhavya Shah <bhavyabond001@gmail.com>
2026-07-28 00:11:44 +0530

    Time: 0 ms (100%), Space: 9.1 MB (13.27%) - LeetHub

---
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.c
@@ -0,0 +1,15 @@
+int maxProduct(int* nums, int numsSize) {
+    int i=0,max1=0,max2=0;
+
+    for(i=0;i<numsSize;i++){
+        if(max1<nums[i]){
+            max2=max1;
+            max1=nums[i];
+        }
+        else if(max2<nums[i]){
+            max2=nums[i];
+        }
+    }
+
+    return (max1-1)*(max2-1);
+}
