a2fea917347ce1b5c1f80929fa29d290e941ccb3
Bhavya Shah <bhavyabond001@gmail.com>
2026-08-20 00:24:26 +0530

    Time: 0 ms (100%), Space: 9 MB (46.32%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.c
@@ -0,0 +1,35 @@
+bool search(int* nums, int numsSize, int target) {
+    int low=0,high=numsSize-1,mid=0;
+
+    while(low<=high){
+        mid=low+(high-low)/2;
+        
+
+        if(nums[mid]==target){
+            return true;
+        }
+
+        if(nums[mid]==nums[high]){
+            high--;
+            continue;
+        }
+
+        if(nums[mid]<nums[high]){
+            if(nums[mid]<=target && target<=nums[high]){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        else{
+            if(nums[low]<=target && target<=nums[mid]){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+    }
+    return false;
+}
